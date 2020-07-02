3d8918275f4edf798bcc659fff8f20d5912f77ae
Sergey <sskryl@mail.ru>
2020-07-02 16:54:37 +0300

    practic1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log(1);
+let numbersOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?"),
+    a = prompt("Какой последний фильм Вы посмотрели?"),
+    b = prompt("Во сколько Вы его оцените?"),
+    c = prompt("Какой последний фильм Вы посмотрели?"),
+    d = prompt("Во сколько Вы его оцените?"),
+    personalMovieDB = {
+        count: numbersOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        private: false
+    };
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
